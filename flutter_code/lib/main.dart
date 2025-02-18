3e68d2e9552678d37028f545d71ef7bdeea22613
Farrukh Saif <syedfarrukhsaif@gmail.com>
2025-02-17 23:04:51 -0500

    Remove DIY navigation

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'screens/photo_select.dart';
 import 'screens/apply_effects.dart';
@@ -13,52 +14,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Image Editor',
       theme: ThemeData.dark(),
-      home: const MainNavigator(),
+      home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MainNavigator extends StatefulWidget {
-  const MainNavigator({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  @override
-  State<MainNavigator> createState() => _MainNavigatorState();
-}
+  void _navigateToApplyEffects(BuildContext context, Uint8List imageBytes) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ApplyEffectsScreen(
+          imageBytes: imageBytes,
+          onBack: () => Navigator.pop(context),
+          onDone: (resultBytes) => _navigateToFinalResult(context, resultBytes),
+        ),
+      ),
+    );
+  }
 
-class _MainNavigatorState extends State<MainNavigator> {
-  final List<Widget> _pages = [];
-
-  void _pushPage(Widget page) => setState(() => _pages.add(page));
-  void _popPage() => setState(() => _pages.removeLast());
-  void _goHome() => setState(() => _pages.clear());
+  void _navigateToFinalResult(BuildContext context, Uint8List resultBytes) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FinalResultScreen(
+          imageBytes: resultBytes,
+          onHomePressed: () => Navigator.popUntil(context, (route) => route.isFirst),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      pages: [
-        MaterialPage(
-          child: PhotoSelectScreen(
-            onImageSelected: (bytes) => _pushPage(
-              ApplyEffectsScreen(
-                imageBytes: bytes,
-                onBack: _popPage,
-                onDone: (resultBytes) => _pushPage(
-                  FinalResultScreen(
-                    imageBytes: resultBytes,
-                    onHomePressed: _goHome,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        if (_pages.isNotEmpty) ..._pages.map((p) => MaterialPage(child: p))
-      ],
-      onPopPage: (route, result) {
-        if (_pages.isNotEmpty) _popPage();
-        return route.didPop(result);
-      },
+    return Scaffold(
+      body: PhotoSelectScreen(
+        onImageSelected: (imageBytes) => _navigateToApplyEffects(context, imageBytes),
+      ),
     );
   }
 }
